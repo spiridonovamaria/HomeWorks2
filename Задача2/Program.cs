ee44125a7a134b35ba5d61603e9bf1d32d8044fb
Maria Spiridonova <traffic2007@mail.ru>
2022-08-15 17:29:56 +0300

    Task2

---
+++ Задача2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+//и удаляет вторую цифру этого числа.
+
+
+int DeleteMiddle(){    
+    int number = new Random().Next(100,1000);
+    System.Console.WriteLine(number);
+    int rmd = number % 10;
+    return number/100 *10 + rmd;
+    
+}
+System.Console.WriteLine(DeleteMiddle());
