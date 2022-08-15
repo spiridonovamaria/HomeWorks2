d2eee402a6cdc585ee02aa776e61506f346c4c51
Maria Spiridonova <traffic2007@mail.ru>
2022-08-15 17:23:46 +0300

    Task1

---
+++ Задача1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string message){     //Трехзначное число
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    if (result < 100 || result > 999){
+        throw new Exception("Число не трехзначное!");
+    } else 
+        return result;
+}
+int FineMiddleNumber( int result){      // Вторая цифра числа
+    int rmd = result/10 % 10;
+    return rmd;
+}
+
+System.Console.WriteLine(FineMiddleNumber(Prompt("Введите число: ")));
