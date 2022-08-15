4b57620a4992a49c271ebe1ad30a0d2f4c38d9cd
Maria Spiridonova <traffic2007@mail.ru>
2022-08-15 17:35:15 +0300

    Task4

---
+++ Задача4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+
+byte Prompt(string message){      
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    byte result = byte.Parse(value);
+    if (result < 1 || result >7){
+        throw new Exception($"В неделе семь дней, а не {result}!!!");
+    } else 
+        return result;
+}
+string IsAWeekDay(byte number){   
+    return (number == 6 || number == 7)? "Да" : "Нет";
+}
+System.Console.WriteLine(IsAWeekDay(Prompt("Введите число, обозначающее день недели: ")));
